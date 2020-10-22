b40ed0fefd35c95af9cdd9e8984eb6f04c47c714
koushith <koushith97@gmail.com>
2020-10-23 02:52:57 +0530

    code refactors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello', app: 'test' });
@@ -24,7 +24,34 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req);
+  // console.log(req.body);
+  // figure out the id for new obj, ususlly this is taken care of database.
+  // this will find the lendth of all id. and in the end it will add 1- eg: len -9, new id= 9+1 10. post-we are creating
+  const newId = tours[tours.length - 1].id + 1;
+
+  // assign the new id along with the bodt to the data
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  // push this to the tours data
+  tours.push(newTour);
+
+  // write to tours- before we were only reading- this will be a js object- convert it to json (Overrides)
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'written successfuly',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 app.listen(3000, () => {
   console.log('APp running on port 3000');
