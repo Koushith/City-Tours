171bd3ae31098b2d8707f11401c2a6026099cfa9
koushith <koushith97@gmail.com>
2020-10-29 01:09:00 +0530

    adds mvc pattern

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const morgan = require('morgan');
 
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
@@ -17,149 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'hello', app: 'test' });
-//   console.log(req.url);
-// });
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// Route handelers
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //converts string to number- we can also use parse int
-
-  // send 404 if the id > length of tours
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const tour = tours.find((e) => e.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-// create tour
-const createTour = (req, res) => {
-  //console.log(req);
-  // console.log(req.body);
-  // figure out the id for new obj, ususlly this is taken care of database.
-  // this will find the lendth of all id. and in the end it will add 1- eg: len -9, new id= 9+1 10. post-we are creating
-  const newId = tours[tours.length - 1].id + 1;
-
-  // assign the new id along with the bodt to the data
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  // push this to the tours data
-  tours.push(newTour);
-
-  // write to tours- before we were only reading- this will be a js object- convert it to json (Overrides)
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'written successfuly',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-// get users
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not yet implimented',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not yet implimented',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not yet implimented',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not yet implimented',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route has not yet implimented',
-  });
-};
-
-// Routes
-//app.get('/api/v1/tours', getAllTours);
-
-// get by param-id -get one tour
-//app.get('/api/v1/tours/:id', getTour);
-
-// create new tour
-//app.post('/api/v1/tours', createTour);
-
-// update and delete are same- just change the status code and method.
-
-// app.patch('/api/vi/tours/:id', (req, res)=>{
-//    if (req.params.id*1 > tours.length) {
-//    return res.status(404).json({
-//     status: 'Fail',
-//        message: 'Invalid ID',
-//      });
-//    }
-//    res.status(200).json({
-//      status: 'Success',
-//      data: {
-//        tour: "<Upadeyd>"
-//      },
-//  })
-
-// chaining routes- clean code
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour);
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app.route('/api/v1/user/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-// Start server
-app.listen(3000, () => {
-  console.log('App started on port 3000');
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+// Start server
+app.listen(3000, () => {
+  console.log('App started on port 3000');
+});
